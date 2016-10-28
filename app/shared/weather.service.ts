3efbbdc721a2ae730efbd6ca77454f455409f3df
Michael Asimakopoulos <m.asimakopoulos@gmail.com>
2016-10-28 08:50:25 +0300

    dasksky api key is now configurable

--- app/shared/weather.service.ts
+++ app/shared/weather.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 import { Observable } from "rxjs/Rx";
 import "rxjs/add/operator/map";
+import { Config } from './../app.config';
 
 @Injectable()
 export class WeatherService {
-  constructor(private _http: Http) {}
+  constructor(private _http: Http, private _config: Config) {}
 
   getCurrentTemperature(latitude: number, longitude: number) {
     console.log(latitude, longitude);
-    return this._http.get(`https://api.darksky.net/forecast/***REMOVED***/${latitude},${longitude}/?units=si`)
+    return this._http.get(`https://api.darksky.net/forecast/${this._config.apiKeys.darksky}/${latitude},${longitude}/?units=si`)
       .map(res => res.json())
       .map(d => d.currently.temperature)
       .catch(this.handleErrors);
